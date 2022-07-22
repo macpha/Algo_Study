1963d2eea8b515134a0f00c0e8cf548d75f7b5c1
macpha <46174201+macpha@users.noreply.github.com>
2022-07-22 22:46:21 +0900

    [Silver III] Title: N과 M (1), Time: 212 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+arr = []
+
+def dfs():
+    if len(arr) == M:
+        print(' '.join(map(str, arr)))
+        return
+    
+    for i in range(1, N + 1):
+        if i not in arr:
+            arr.append(i)
+            dfs()
+            arr.pop()
+
+dfs()
