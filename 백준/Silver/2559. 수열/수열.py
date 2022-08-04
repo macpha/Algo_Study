9731fb0ee5cf83b72c08935473565fb39ddeadd2
macpha <46174201+macpha@users.noreply.github.com>
2022-08-04 20:22:02 +0900

    [Silver III] Title: 수열, Time: 108 ms, Memory: 39572 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -4,15 +4,13 @@ input = sys.stdin.readline
 N, K = map(int, input().split())
 
 arr = list(map(int, input().split()))
-sum_arr = [0] * (N + 1)
 
-sum_arr[1] = arr[0]
-for _ in range(2, N + 1):
-    sum_arr[_] = sum_arr[_ - 1] + arr[_ - 1]
+sum_arr = sum(arr[:K])
+result = sum_arr
 
-result = []
-for i in range(N - K + 1):
-    result.append(sum_arr[i + K] - sum_arr[i])
+for i in range(N - K):
+    sum_arr = sum_arr - arr[i] + arr[i + K]
+    if result < sum_arr:
+        result = sum_arr
 
-print(max(result))
-    
+print(result)
